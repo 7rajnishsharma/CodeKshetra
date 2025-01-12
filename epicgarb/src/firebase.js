906b4e22aaa3593ff68d4991bd0e684919e4bc42
Rajnish Sharma <111423953+7rajnishsharma@users.noreply.github.com>
2025-01-12 19:08:04 +0530

    Update firebase.js

--- epicgarb/src/firebase.js
+++ epicgarb/src/firebase.js
@@ -3,13 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCiz8qFjbtKHi7D-6R4YgGi3sypnsIaCnw",
-  authDomain: "epic-garb.firebaseapp.com",
-  projectId: "epic-garb",
-  storageBucket: "epic-garb.appspot.com",
-  messagingSenderId: "417218333049",
-  appId: "1:417218333049:web:2a329d2c1401c196ddc7a8",
-  measurementId: "G-QCMP7GEL20"
+// Your API key 
 };
 
 // Initialize Firebase
